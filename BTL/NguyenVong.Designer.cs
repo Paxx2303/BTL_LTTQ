7dd282c74d98678fd93b620cb211da018cd5eb5b
hellod3d <140498065+hellod3d@users.noreply.github.com>
2024-10-20 07:53:56 +0700

    Add files via upload

---
+++ BTL/NguyenVong.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace BTL
+{
+    partial class NguyenVong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button7 = new Button();
+            button4 = new Button();
+            lblTen = new Label();
+            label2 = new Label();
+            btnHome = new Button();
+            btnPreferences = new Button();
+            btnChangePassword = new Button();
+            panel2 = new Panel();
+            btnSupport = new Button();
+            btnSignOut = new Button();
+            btnPayment = new Button();
+            panel1 = new Panel();
+            panel5 = new Panel();
+            dataGridView1 = new DataGridView();
+            btnThemNV = new Button();
+            btnCapNhat = new Button();
+            btnXoa = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button7
+            // 
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.ForeColor = SystemColors.ControlText;
+            button7.Image = Properties.Resources.icons8_find_24;
+            button7.ImageAlign = ContentAlignment.MiddleLeft;
+            button7.Location = new Point(760, 9);
+            button7.Margin = new Padding(4);
+            button7.Name = "button7";
+            button7.Size = new Size(178, 46);
+            button7.TabIndex = 20;
+            button7.Text = "      Grade lookup";
+            button7.TextAlign = ContentAlignment.MiddleLeft;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.GrayText;
+            button4.Image = Properties.Resources.icons8_user_50;
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(946, 2);
+            button4.Margin = new Padding(4);
+            button4.Name = "button4";
+            button4.Size = new Size(60, 60);
+            button4.TabIndex = 14;
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // lblTen
+            // 
+            lblTen.AutoSize = true;
+            lblTen.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTen.Location = new Point(1012, 30);
+            lblTen.Margin = new Padding(4, 0, 4, 0);
+            lblTen.Name = "lblTen";
+            lblTen.Size = new Size(153, 20);
+            lblTen.TabIndex = 5;
+            lblTen.Text = "Nguyễn Thị Vân Anh";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(1012, 10);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Xin chào,";
+            // 
+            // btnHome
+            // 
+            btnHome.FlatAppearance.BorderSize = 0;
+            btnHome.FlatStyle = FlatStyle.Flat;
+            btnHome.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHome.ForeColor = SystemColors.GrayText;
+            btnHome.Image = Properties.Resources.icons8_building_100;
+            btnHome.ImageAlign = ContentAlignment.MiddleLeft;
+            btnHome.Location = new Point(-1, -1);
+            btnHome.Margin = new Padding(4);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(289, 86);
+            btnHome.TabIndex = 21;
+            btnHome.Text = "                 Home";
+            btnHome.TextAlign = ContentAlignment.MiddleLeft;
+            btnHome.UseVisualStyleBackColor = true;
+            btnHome.Click += btnHome_Click;
+            // 
+            // btnPreferences
+            // 
+            btnPreferences.FlatAppearance.BorderSize = 0;
+            btnPreferences.FlatStyle = FlatStyle.Flat;
+            btnPreferences.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPreferences.ForeColor = SystemColors.GrayText;
+            btnPreferences.Image = Properties.Resources.icons8_create_64;
+            btnPreferences.ImageAlign = ContentAlignment.TopLeft;
+            btnPreferences.Location = new Point(8, 106);
+            btnPreferences.Margin = new Padding(4);
+            btnPreferences.Name = "btnPreferences";
+            btnPreferences.Size = new Size(280, 86);
+            btnPreferences.TabIndex = 22;
+            btnPreferences.Text = "               Preferences";
+            btnPreferences.TextAlign = ContentAlignment.MiddleLeft;
+            btnPreferences.UseVisualStyleBackColor = true;
+            btnPreferences.Click += btnPreferences_Click;
+            // 
+            // btnChangePassword
+            // 
+            btnChangePassword.FlatAppearance.BorderSize = 0;
+            btnChangePassword.FlatStyle = FlatStyle.Flat;
+            btnChangePassword.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnChangePassword.ForeColor = SystemColors.GrayText;
+            btnChangePassword.Image = Properties.Resources.icons8_change_password_58;
+            btnChangePassword.ImageAlign = ContentAlignment.MiddleLeft;
+            btnChangePassword.Location = new Point(8, 200);
+            btnChangePassword.Margin = new Padding(4);
+            btnChangePassword.Name = "btnChangePassword";
+            btnChangePassword.Size = new Size(280, 86);
+            btnChangePassword.TabIndex = 24;
+            btnChangePassword.Text = "               Password";
+            btnChangePassword.TextAlign = ContentAlignment.MiddleLeft;
+            btnChangePassword.UseVisualStyleBackColor = true;
+            btnChangePassword.Click += btnChangePassword_Click;
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(btnHome);
+            panel2.Controls.Add(btnSupport);
+            panel2.Controls.Add(btnSignOut);
+            panel2.Controls.Add(btnPreferences);
+            panel2.Controls.Add(btnChangePassword);
+            panel2.Controls.Add(btnPayment);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 68);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(294, 580);
+            panel2.TabIndex = 32;
+            // 
+            // btnSupport
+            // 
+            btnSupport.FlatAppearance.BorderSize = 0;
+            btnSupport.FlatStyle = FlatStyle.Flat;
+            btnSupport.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSupport.ForeColor = SystemColors.GrayText;
+            btnSupport.Image = Properties.Resources.icons8_support_50;
+            btnSupport.ImageAlign = ContentAlignment.MiddleLeft;
+            btnSupport.Location = new Point(17, 399);
+            btnSupport.Margin = new Padding(4);
+            btnSupport.Name = "btnSupport";
+            btnSupport.Size = new Size(271, 86);
+            btnSupport.TabIndex = 25;
+            btnSupport.Text = "             Support";
+            btnSupport.TextAlign = ContentAlignment.MiddleLeft;
+            btnSupport.UseVisualStyleBackColor = true;
+            btnSupport.Click += btnSupport_Click;
+            // 
+            // btnSignOut
+            // 
+            btnSignOut.FlatAppearance.BorderSize = 0;
+            btnSignOut.FlatStyle = FlatStyle.Flat;
+            btnSignOut.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSignOut.ForeColor = SystemColors.GrayText;
+            btnSignOut.Image = Properties.Resources.icons8_exit_48;
+            btnSignOut.ImageAlign = ContentAlignment.MiddleLeft;
+            btnSignOut.Location = new Point(17, 493);
+            btnSignOut.Margin = new Padding(4);
+            btnSignOut.Name = "btnSignOut";
+            btnSignOut.Size = new Size(271, 86);
+            btnSignOut.TabIndex = 26;
+            btnSignOut.Text = "             Sign Out";
+            btnSignOut.TextAlign = ContentAlignment.MiddleLeft;
+            btnSignOut.UseVisualStyleBackColor = true;
+            btnSignOut.Click += btnSignOut_Click;
+            // 
+            // btnPayment
+            // 
+            btnPayment.FlatAppearance.BorderSize = 0;
+            btnPayment.FlatStyle = FlatStyle.Flat;
+            btnPayment.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPayment.ForeColor = SystemColors.GrayText;
+            btnPayment.Image = Properties.Resources.icons8_payment_64;
+            btnPayment.ImageAlign = ContentAlignment.MiddleLeft;
+            btnPayment.Location = new Point(17, 294);
+            btnPayment.Margin = new Padding(4);
+            btnPayment.Name = "btnPayment";
+            btnPayment.Size = new Size(271, 86);
+            btnPayment.TabIndex = 23;
+            btnPayment.Text = "             Payment";
+            btnPayment.TextAlign = ContentAlignment.MiddleLeft;
+            btnPayment.UseVisualStyleBackColor = true;
+            btnPayment.Click += btnPayment_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(128, 128, 255);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(lblTen);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(panel5);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1197, 68);
+            panel1.TabIndex = 31;
+            // 
+            // panel5
+            // 
+            panel5.BorderStyle = BorderStyle.FixedSingle;
+            panel5.Location = new Point(297, 68);
+            panel5.Margin = new Padding(0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(899, 580);
+            panel5.TabIndex = 19;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.HighlightText;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.GridColor = SystemColors.Control;
+            dataGridView1.Location = new Point(351, 297);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(825, 327);
+            dataGridView1.TabIndex = 33;
+            // 
+            // btnThemNV
+            // 
+            btnThemNV.Location = new Point(1029, 120);
+            btnThemNV.Name = "btnThemNV";
+            btnThemNV.Size = new Size(147, 49);
+            btnThemNV.TabIndex = 34;
+            btnThemNV.Text = "Thêm nguyện vọng";
+            btnThemNV.UseVisualStyleBackColor = true;
+            // 
+            // btnCapNhat
+            // 
+            btnCapNhat.Location = new Point(1029, 175);
+            btnCapNhat.Name = "btnCapNhat";
+            btnCapNhat.Size = new Size(147, 49);
+            btnCapNhat.TabIndex = 35;
+            btnCapNhat.Text = "Cập nhật";
+            btnCapNhat.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(1029, 230);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(147, 47);
+            btnXoa.TabIndex = 36;
+            btnXoa.Text = "Xoá";
+            btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(351, 120);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(641, 34);
+            textBox1.TabIndex = 37;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Location = new Point(351, 175);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(641, 34);
+            textBox2.TabIndex = 38;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Location = new Point(351, 227);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(641, 34);
+            textBox3.TabIndex = 39;
+            // 
+            // NguyenVong
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1197, 648);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(btnXoa);
+            Controls.Add(btnCapNhat);
+            Controls.Add(btnThemNV);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            Name = "NguyenVong";
+            SizeGripStyle = SizeGripStyle.Show;
+            Text = "NguyenVong";
+            panel2.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button7;
+        private Button button4;
+        private Label lblTen;
+        private Label label2;
+        private Button btnHome;
+        private Button btnPreferences;
+        private Button btnChangePassword;
+        private Panel panel2;
+        private Button btnSupport;
+        private Button btnSignOut;
+        private Button btnPayment;
+        private Panel panel1;
+        private Panel panel5;
+        private DataGridView dataGridView1;
+        private Button btnThemNV;
+        private Button btnCapNhat;
+        private Button btnXoa;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ BTL/NguyenVong.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL
+{
+    public partial class NguyenVong : Form
+    {
+        public NguyenVong()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            UserInterface HoSo = new UserInterface();
+            HoSo.ShowDialog();
+            this.Close();
+        }
+
+        private void btnChangePassword_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ChangePassword changePassword = new ChangePassword();
+            changePassword.ShowDialog();
+            this.Close();
+            
+        }
+
+        private void btnPayment_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Payment payment = new Payment();
+            payment.ShowDialog();
+            this.Close();
+        }
+
+        private void btnSupport_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSignOut_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login login = new Login();
+            login.ShowDialog();
+            this.Close();
+        }
+
+        private void btnPreferences_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
